70f63fa90890ff623785196b729b32b0303f60cd
elishowk <elishowk@nonutc.fr>
2011-09-14 15:17:21 +0200

    updated

--- ucengine.sh
+++ ucengine.sh
@@ -1,14 +1,16 @@
 #!/bin/sh
 cd
 git clone git://github.com/AF83/ucengine.git
+git checkout develop
 cd ucengine
 make rel
 cd rel/ucengine
-echo "Please enter the virtual host of this service (without http://):"
-read vhost
-sed -i 's/{port, 5280}/{port, 8080}/' etc/uce.cfg
-sed -i 's/{"localhost",/{"'$vhost'",/' etc/uce.cfg
+#echo "Please enter the virtual host of this service (without http://):"
+#read vhost
+cp /tmp/code/uce.cfg etc/uce.cfg
+#sed -i 's/{port, 5280}/{port, 8080}/' etc/uce.cfg
+#sed -i 's/{"localhost",/{"'$vhost'",/' etc/uce.cfg
 bin/ucengine start
 sleep 3
 bin/ucengine ping
-bin/demo.sh $vhost
+bin/demo.sh localhost
