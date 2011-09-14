21410e4e8883e334d8088da57084b1c01fb7c2db
elishowk <elishowk@nonutc.fr>
2011-09-14 15:25:08 +0200

    dump debug uce.cfg

--- ucengine.sh
+++ ucengine.sh
@@ -7,6 +7,7 @@ make rel
 cd rel/ucengine
 #echo "Please enter the virtual host of this service (without http://):"
 #read vhost
+cat /tmp/code/uce.cfg
 cp /tmp/code/uce.cfg etc/uce.cfg
 #sed -i 's/{port, 5280}/{port, 8080}/' etc/uce.cfg
 #sed -i 's/{"localhost",/{"'$vhost'",/' etc/uce.cfg
